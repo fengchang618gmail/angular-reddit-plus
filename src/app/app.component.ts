b1060f99f871e76d0a2860ce0fe500a83078be0f
Grace Feng <g.feng@sap.com>
2018-01-10 14:13:35 +0800

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  articles: Article[];
+  article: Article;
+
+  constructor() {
+    this.articles = [
+      new Article('Angualr 5', 'http://angualr.io', 10),
+      new Article('Fullstack', 'http://fullstack.io', 10),
+      new Article('Angualr Homepage', 'http://angualr.io', 10),
+    ];
+  }
+
+  addArticle(newtitle, newlink): boolean {
+    this.articles.push(new Article(`${newtitle.value}`, `${newlink.value}`, 10));
+    newtitle.value = '';
+    newlink.value = '';
+    console.log(`${newtitle.value}`);
+    return false;
+  }
+
+  sortedArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
